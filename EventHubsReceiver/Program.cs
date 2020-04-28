10cf219e11fe71f86e6c7d325c0e50daac080569
arnab_mohit <Khandokar.Arnab@brainstation23.com>
2020-04-28 17:53:17 +0600

    First commit

---
+++ EventHubsReceiver/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+
+namespace EventHubsReceiver
+{
+    class Program
+    {
+        private const string connectionString = "Endpoint=sb://arnabnamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=5qZYY89SoWAcq7Lzw3qzYxonT0+J2Hm3OWoaQRn7F64=";
+        private const string eventHubName = "arnabeventhub";
+        private const string blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=arnabdiboshstorage;AccountKey=r8YPi/Pg/1r3kRubg1M8w7ea9jmgqZ2ueDJPkRDSiJsa2Yyxg26WW1n9XF75PJWKDcVumBk6xVNIKkYxIOZ/Rw==;EndpointSuffix=core.windows.net";
+        private const string blobContainerName = "arnabcontainer";
+
+        static async Task Main()
+        {
+            // Read from the default consumer group: $Default
+            string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+            // Create a blob container client that the event processor will use 
+            BlobContainerClient storageClient = new BlobContainerClient(blobStorageConnectionString, blobContainerName);
+
+            // Create an event processor client to process events in the event hub
+            EventProcessorClient processor = new EventProcessorClient(storageClient, consumerGroup, connectionString, eventHubName);
+
+            // Register handlers for processing events and handling errors
+            processor.ProcessEventAsync += ProcessEventHandler;
+            processor.ProcessErrorAsync += ProcessErrorHandler;
+
+            // Start the processing
+            await processor.StartProcessingAsync();
+
+            // Wait for 10 seconds for the events to be processed
+            await Task.Delay(TimeSpan.FromSeconds(10));
+
+            // Stop the processing
+            await processor.StopProcessingAsync();
+        }
+
+        static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+        {
+            // Write the body of the event to the console window
+            Console.WriteLine("\tRecevied event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+
+            // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+            await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+        }
+
+        static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+        {
+            // Write details about the error to the console window
+            Console.WriteLine($"\tPartition '{ eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+            Console.WriteLine(eventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
